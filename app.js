d9e298ffed8d7856fc46fb1d366c8fc5375e9989
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-11 23:56:10 +0200

    add comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// require dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,8 +7,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
+// bootstrap our app
 var app = express();
 
 // view engine setup
@@ -22,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// our basic route to serve the index page
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +56,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
