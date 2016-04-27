bdc8d07c51758b7b26d10c2accb5af1f6613203e
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-27 13:55:30 +0200

    add instructions from tutorial

--- app.js
+++ app.js
@@ -7,11 +7,33 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const routes = require('./routes/index');
 
 // bootstrap our app
 const app = express();
+
+let mongoUrl = '';
+
+// check if the app is running in the cloud and set the MongoDB settings accordingly
+if (process.env.VCAP_SERVICES) {
+  const vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+  mongoUrl = vcapServices.mongodb[0].credentials.uri;
+} else {
+  mongoUrl = 'mongodb://localhost/db';
+}
+
+// connect to our MongoDB
+mongoose.connect(mongoUrl);
+
+// create a Mongoose schema for kittens
+const kittenSchema = mongoose.Schema({
+  name: 'string'
+});
+
+// create a Mongoose model out of our kitten schema
+const Kitten = mongoose.model('Kitten', kittenSchema);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +49,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // our basic route to serve the index page
 app.use('/', routes);
+
+// the route where you can retrieve all the kittens in our MongoDB
+app.post('/create-kitten', (req, res, next) => {
+  const kitten = new Kitten({
+    name: req.query.name
+  });
+
+  kitten.save((err, newKitten) => {
+    if (err) return next(err);
+
+    res.send('Kitten ' + newKitten.name + ' saved');
+  })
+})
+
+// the route where you can retrieve all the kittens in our MongoDB
+app.get('/db', (req, res, next) => {
+  Kitten.find((err, kittens) => {
+    if (err) return next(err);
+
+    res.json(kittens);
+  })
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Congratulations!' });
+  res.render('index', { title: 'I am awesome!' });
 });
 
 module.exports = router;
